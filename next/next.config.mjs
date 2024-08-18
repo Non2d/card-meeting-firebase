222c26df2ffbdeafbdff98b59bab45cc077c3b09
onedanijo11@gmail.com <onedanijo11@gmail.comy>
2024-08-18 21:31:06 +0900

    chore: Remove commented code in next.config.mjs file

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? '/card-meet' : '',
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     basePath: process.env.NODE_ENV === 'production' ? '/card-meet' : '',
+// };
 
-export default nextConfig;
+// export default nextConfig;
