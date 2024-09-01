ffc7d6e1e8884bd7377deb4f892bf4c8c38ce92e
onedanijo11@gmail.com <onedanijo11@gmail.comy>
2024-09-01 19:45:04 +0900

    Remove legacy API and added anonymous auth

--- fastapi/legacy/api/schemas/auth.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class AuthTokenResponse(BaseModel):
-    authToken: str
--- next/components/utils/firebase.tsx
+++ next/components/utils/firebase.tsx
@@ -1,16 +1,32 @@
-import { getApp, getApps, initializeApp } from 'firebase/app'
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPiJMyuPP3EvHePzP2CH8x48t7WTHOB2M",
-  authDomain: "card-meeting-firebase.firebaseapp.com",
-  databaseURL: "https://card-meeting-firebase-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "card-meeting-firebase",
-  storageBucket: "card-meeting-firebase.appspot.com",
-  messagingSenderId: "700011609528",
-  appId: "1:700011609528:web:ae36a39fab7ef565bea34d",
-  measurementId: "G-1PG06DHXSY"
+  apiKey: "AIzaSyCQzjo3nnA6dCmnKga9J_DLCLImwdo97s0",
+  authDomain: "card-game-db-db944.firebaseapp.com",
+  databaseURL: "https://card-game-db-db944-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "card-game-db-db944",
+  storageBucket: "card-game-db-db944.appspot.com",
+  messagingSenderId: "1034499730169",
+  appId: "1:1034499730169:web:4083b9b36401997b5b5028"
 };
 
 // Initialize Firebase
-export const initializeFirebaseApp = () =>
-    getApps().length ? getApp():initializeApp(firebaseConfig);
+export const initializeFirebaseApp = () => {
+  const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  signInAnonymously(auth)
+    .then(() => {
+      console.log('Signed in anonymously');
+      // Additional logic after successful sign-in...
+    })
+    .catch((error: any) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(`Error during anonymous sign-in: ${errorCode} - ${errorMessage}`);
+      // Additional error handling...
+    });
+
+  return app;
+};
